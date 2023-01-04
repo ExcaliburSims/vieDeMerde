0f01acfe7ea51e10c5dba2d9e952fbc339577d63
ExcaliburSims <ximeonzilu@gmail.com>
2023-01-04 15:48:16 +0100

    fix it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import ConnectModal from "./components/ConnectModal";
 
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <div className="container mx-auto mt-5 max-w-2xl pt-0 p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <ConnectModal />
+      </div>
+      <div className="container mx-auto mt-5 max-w-2xl pt-0 p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+        2
+      </div>
+    </div>
+  );
 };
 
 export default App;
