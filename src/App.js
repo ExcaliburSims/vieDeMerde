d30e79b53c51062474768d8e5d1027815f4b9d25
ExcaliburSims <ximeonzilu@gmail.com>
2023-01-06 13:01:26 +0100

    feat: move header modal

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ConnectModal from "./components/ConnectModal";
 const App = () => {
   return (
     <div>
-      <div className="container mx-auto mt-5 max-w-2xl h-64 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+      <div className="container mx-auto mt-10 max-w-2xl h-64 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
         <ConnectModal />
       </div>
       <div className="container mx-auto mt-5 max-w-2xl pt-0 p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
--- src/components/ConnectModal.js
+++ src/components/ConnectModal.js
@@ -3,12 +3,12 @@ import React from "react";
 function ConnectModal() {
   return (
     <>
-      <div className="bg-backg h-8">
-        <button className="font-medium text-gray-500 px-2 text-xl">
+      <div className="bg-red relative bottom-7">
+        <button className="font-medium text-black-500 px-2 text-xl bg-red-600 dark:bg-gray-800 rounded-md border border-transparent">
           {/* className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900" */}
           S'inscrire
         </button>
-        <button className="font-medium text-gray-500 px-2 text-xl">
+        <button className="font-medium text-black-500 px-2 text-xl">
           {/*  className="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700" */}
           Se connecter
         </button>
