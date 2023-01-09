308dd545621fdaa245c9501c765f44a12b207e6c
ExcaliburSims <ximeonzilu@gmail.com>
2023-01-09 12:24:35 +0100

    feat: make title signup

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <div className="container bg-red-900">
+      <div className="flex flex-col items-center justify-center">
+        <h3 className="text-2xl text-white">S'inscrire</h3>
+        <form>
+          <input />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
