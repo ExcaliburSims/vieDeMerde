14fb287b0e36d3c994e324bcf2b2ceae4d0f38f2
ExcaliburSims <ximeonzilu@gmail.com>
2023-02-15 09:32:02 +0100

    feat: add style signup

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,11 +1,21 @@
 import React from "react";
 
 const Signup = () => {
+  const registerEmail = React.useRef();
+  const registerPassword = React.useRef();
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    console.log(registerEmail.current.value, registerPassword.current.value);
+  };
   return (
     <div className="container">
       <div className="flex flex-col items-center justify-center">
         <h3 className="text-2xl text-white">S'inscrire</h3>
-        <form className="flex flex-col items-center justify-center">
+        <form
+          className="flex flex-col items-center justify-center"
+          onSubmit={(e) => handleRegister(e)}
+        >
           <input
             className="rounded-xl border-2 border-gray-600  shadow-sm focus:border-purple-500 focus:ring focus:ring-purple-500 focus:ring-opacity-50 bg-transparent p-2 px-4 mt-3 text-xl text-white"
             placeholder="Pseudo"
@@ -17,12 +27,14 @@ const Signup = () => {
             placeholder="Email"
             type="email"
             required
+            ref={registerEmail}
           />
           <input
             className="rounded-xl border-2 border-gray-600  shadow-sm focus:border-purple-500 focus:ring focus:ring-purple-500 focus:ring-opacity-50 bg-transparent p-2 px-4 mt-3 text-xl text-white"
             placeholder="Mot de passe"
             type="password"
             required
+            ref={registerPassword}
           />
           <input
             className="rounded-xl border-2 border-gray-600  shadow-sm focus:border-purple-500 focus:ring focus:ring-purple-500 focus:ring-opacity-50 bg-amber-600 p-2 px-4 mt-10 text-xl text-white"
